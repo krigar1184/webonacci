9ef925514586ef83bc691ed88af4625800e2a5ec
Nikita Antonenkov <krigar1184@gmail.com>
2018-12-04 22:39:06 +0300

    Basic implementation

--- fibonacci/app.py
+++ fibonacci/app.py
@@ -4,6 +4,41 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def nth_fibonacci_number(n):  # TODO more efficient algorithm needed
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    return nth_fibonacci_number(n - 1) + nth_fibonacci_number(n - 2)
+
+
+def f(start: int, end: int) -> list:
+    assert start >= 0, 'start should be less than or equal to zero'
+    assert start < end, 'start index should be less than end index'
+
+    start_value = nth_fibonacci_number(start)
+    next_value = nth_fibonacci_number(start + 1)
+    counter = end - start
+    result = [start_value, next_value]
+
+    while counter > 1:
+        tmp = start_value
+        start_value = next_value
+        next_value = tmp + next_value
+
+        result.append(next_value)
+        counter -= 1
+
+    return result
+
+
 @app.route('/health')
 def health_check():
-    return 'healthcheck'
+    return 'OK'
+
+
+@app.route('/fib/<int:start_idx>/<int:end_idx>')
+def fib(start_idx, end_idx):
+    return f'Result: {fibonacci(start_idx, end_idx)}'
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,5 +1,11 @@
-from fibonacci import __version__
+import pytest
+from fibonacci.app import f
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.mark.parametrize(('start', 'end', 'expected'), [
+    (0, 1, [0, 1]),
+    (3, 5, [2, 3, 5]),
+    (0, 7, [0, 1, 1, 2, 3, 5, 8, 13])
+])
+def test_f(start, end, expected):
+    assert f(start, end) == expected
